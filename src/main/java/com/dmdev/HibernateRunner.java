82b3d22d889d0044a14fc31ed956523b95a7f07c
parom <E.Parominskiy@isida.by>
2023-09-25 10:06:38 +0300

    Configuration Entity fields @Column

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -1,12 +1,15 @@
 package com.dmdev;
 
+import com.dmdev.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.concurrent.BlockingDeque;
 
 public class HibernateRunner {
@@ -22,12 +25,29 @@ public class HibernateRunner {
 
 
         Configuration configuration = new Configuration();
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());// first var , second var @Column above field
 //        configuration.configure("path/to/config.xml");
+//        configuration.addAnnotatedClass(User.class);// first var mapping entity, second var add <mapping class= to xml >
         configuration.configure();
 
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
             System.out.println("ok");
+
+            User user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 19))
+                    .age(22)
+                    .build();
+
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit(); // if everything is goog
+//            session.getTransaction().rollback(); // if something wrong
+
+
         }
 
     }
---
+++ src/main/java/com/dmdev/entity/User.java
@@ -0,0 +1,105 @@
+package com.dmdev.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users", schema = "public")
+public class User {
+
+    @Id
+    private String username;
+    private String firstname;
+    private String lastname;
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+    private Integer age;
+
+//    public User() {
+//    }
+//
+//    public User(String username, String firstname, String lastname, LocalDate birthDate, Integer age) {
+//        this.username = username;
+//        this.firstname = firstname;
+//        this.lastname = lastname;
+//        this.birthDate = birthDate;
+//        this.age = age;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public void setUsername(String username) {
+//        this.username = username;
+//    }
+//
+//    public String getFirstname() {
+//        return firstname;
+//    }
+//
+//    public void setFirstname(String firstname) {
+//        this.firstname = firstname;
+//    }
+//
+//    public String getLastname() {
+//        return lastname;
+//    }
+//
+//    public void setLastname(String lastname) {
+//        this.lastname = lastname;
+//    }
+//
+//    public LocalDate getBirthDate() {
+//        return birthDate;
+//    }
+//
+//    public void setBirthDate(LocalDate birthDate) {
+//        this.birthDate = birthDate;
+//    }
+//
+//    public Integer getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(Integer age) {
+//        this.age = age;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "User{" +
+//                "username='" + username + '\'' +
+//                ", firstname='" + firstname + '\'' +
+//                ", lastname='" + lastname + '\'' +
+//                ", birthDate=" + birthDate +
+//                ", age=" + age +
+//                '}';
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        User user = (User) o;
+//        return Objects.equals(username, user.username) && Objects.equals(firstname, user.firstname) && Objects.equals(lastname, user.lastname) && Objects.equals(birthDate, user.birthDate) && Objects.equals(age, user.age);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(username, firstname, lastname, birthDate, age);
+//    }
+}
