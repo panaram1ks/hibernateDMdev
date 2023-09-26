ad0638b6260f76781a1e3b8339bfec697f9c6dd3
parom <E.Parominskiy@isida.by>
2023-09-26 08:46:17 +0300

    Logging file appender

--- src/main/java/com/dmdev/HibernateRunner2.java
+++ src/main/java/com/dmdev/HibernateRunner2.java
@@ -1,22 +1,17 @@
 package com.dmdev;
 
-import com.dmdev.converter.BirthdayConverter;
-import com.dmdev.entity.Birthday;
-import com.dmdev.entity.Role;
 import com.dmdev.entity.User;
 import com.dmdev.util.HibernateUtil;
-import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
-import org.hibernate.cfg.Configuration;
-
-import java.sql.SQLException;
-import java.time.LocalDate;
+import org.hibernate.Transaction;
 
 /**
  * @author E.Parominsky 25/09/2023 16:12
  */
+@Slf4j
+//@Log4j
 public class HibernateRunner2 {
 
     public static void main(String[] args) {
@@ -26,15 +21,23 @@ public class HibernateRunner2 {
                 .firstname("Ivan")
                 .build();
         //user is Transient;
+//        log.info("user entity is in transient state, object: " + user);
+        log.info("user entity is in transient state, object: {}", user);
 
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
              Session session1 = sessionFactory.openSession()) {
-            session1.beginTransaction();
+            Transaction transaction = session1.beginTransaction();
+            log.trace("Transaction is created, {}", transaction);
 
             session1.saveOrUpdate(user); // Persistent
+            log.trace("User is in persistent state: {}, session {}", user, session1);
 
             session1.getTransaction().commit();
+        } catch (Exception e) {
+            log.error("Exception occurred", e);
+            throw e;
         }
+        log.warn("User is in detached state: {}, session is closed", user);
 
         try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
              Session session2 = sessionFactory.openSession()) {
