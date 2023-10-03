17c06c4c600719f5f802d256b50c9d852b316bd6
parom <E.Parominskiy@isida.by>
2023-10-03 11:45:55 +0300

    59. Entity Graphs    -   build entity graph in code

--- src/main/java/com/dmdev/ManyToOneRunner.java
+++ src/main/java/com/dmdev/ManyToOneRunner.java
@@ -1,5 +1,6 @@
 package com.dmdev;
 
+import com.dmdev.entity.manytomany.UserChat;
 import com.dmdev.entity.onetomany.User;
 import com.dmdev.util.HibernateUtil;
 import com.dmdev.util.TestDataImporter;
@@ -9,6 +10,7 @@ import org.hibernate.Transaction;
 import org.hibernate.annotations.QueryHints;
 import org.hibernate.graph.GraphSemantic;
 import org.hibernate.graph.RootGraph;
+import org.hibernate.graph.SubGraph;
 
 import java.util.List;
 import java.util.Map;
@@ -23,9 +25,15 @@ public class ManyToOneRunner {
 //            session.enableFetchProfile("withCompany"); // fetch profile works only with separate entity
 //            session.enableFetchProfile("withCompanyAndPayment"); // fetch profile works only with separate entity
 
+            RootGraph<User> userGraph = session.createEntityGraph(User.class);
+            userGraph.addAttributeNodes("company", "userChats");
+            SubGraph<UserChat> userChatSubGraph = userGraph.addSubGraph("userChats", UserChat.class);
+            userChatSubGraph.addAttributeNodes("chat");
+
+
             RootGraph<?> graph = session.getEntityGraph("WithCompanyAndChat");
 
-            Map<String, Object> properties = Map.of(GraphSemantic.LOAD.getJpaHintName(), graph);
+            Map<String, Object> properties = Map.of(GraphSemantic.LOAD.getJpaHintName(), userGraph );
             var user = session.find(User.class, 1L, properties);
 
 //            User user = session.get(User.class, 1L);
@@ -34,7 +42,7 @@ public class ManyToOneRunner {
             System.out.println(user.getUserChats().size());
 
             List<User> users = session.createQuery("select u from User u join fetch u.payments join fetch u.company where 1 = 1", User.class)
-                    .setHint(GraphSemantic.LOAD.getJpaHintName(), graph)
+                    .setHint(GraphSemantic.LOAD.getJpaHintName(), userGraph )
                     .list();
             users.forEach(it -> System.out.println(it.getPayments().size()));
             users.forEach(it -> System.out.println(it.getCompany().getName()));
