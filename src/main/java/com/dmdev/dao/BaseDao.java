75bcf5cb0c7138bd52e7afab7affecff9af5b0d3
parom <E.Parominskiy@isida.by>
2023-10-04 17:43:05 +0300

    BaseDao

--- src/main/java/com/dmdev/dao/BaseDao.java
+++ src/main/java/com/dmdev/dao/BaseDao.java
@@ -1,10 +1,56 @@
 package com.dmdev.dao;
 
 import com.dmdev.entity.BaseEntityInterface;
+import com.dmdev.entity.onetomany.Payment;
+import lombok.Cleanup;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
 import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
 
+@RequiredArgsConstructor
 public abstract class BaseDao<K extends Serializable, E extends BaseEntityInterface<K>>  implements Dao<K, E> {
 
+    private final Class<E> clazz;
+    private final SessionFactory sessionFactory;
 
+    @Override
+    public E save(E entity) {
+        @Cleanup Session session = sessionFactory.openSession();
+        session.save(entity);
+        return entity;
+    }
+
+    @Override
+    public void delete(K id) {
+        @Cleanup Session session = sessionFactory.openSession();
+        session.delete(id);
+        session.flush();
+    }
+
+    @Override
+    public void update(E entity) {
+        @Cleanup Session session = sessionFactory.openSession();
+        session.merge(entity);
+    }
+
+    @Override
+    public Optional<E> findById(K id) {
+        @Cleanup Session session = sessionFactory.openSession();
+        return Optional.ofNullable(session.find(clazz, id));
+    }
+
+    @Override
+    public List<E> findAll() {
+        @Cleanup Session session = sessionFactory.openSession();
+        CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery<E> criteria = cb.createQuery(clazz);
+        criteria.from(clazz);
+        return session.createQuery(criteria).getResultList();
+    }
 }
--- src/main/java/com/dmdev/dao/PaymentDao.java
+++ src/main/java/com/dmdev/dao/PaymentDao.java
@@ -1,49 +1,11 @@
 package com.dmdev.dao;
 
 import com.dmdev.entity.onetomany.Payment;
-import lombok.Cleanup;
-import lombok.RequiredArgsConstructor;
-import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 
-import java.util.List;
-import java.util.Optional;
+public class PaymentDao extends BaseDao<Long, Payment> {
 
-
-@RequiredArgsConstructor
-public class PaymentDao implements Dao<Long, Payment> {
-
-    private final SessionFactory sessionFactory;
-
-    @Override
-    public Payment save(Payment entity) {
-        @Cleanup Session session = sessionFactory.openSession();
-        session.save(entity);
-        return entity;
-    }
-
-    @Override
-    public void delete(Long id) {
-        @Cleanup Session session = sessionFactory.openSession();
-        session.delete(id);
-        session.flush();
-    }
-
-    @Override
-    public void update(Payment entity) {
-        @Cleanup Session session = sessionFactory.openSession();
-        session.merge(entity);
-    }
-
-    @Override
-    public Optional<Payment> findById(Long id) {
-        @Cleanup Session session = sessionFactory.openSession();
-        return Optional.ofNullable(session.find(Payment.class, id));
-    }
-
-    @Override
-    public List<Payment> findAll() {
-        @Cleanup Session session = sessionFactory.openSession();
-        return session.createQuery("select p from Payment p", Payment.class).getResultList();
+    public PaymentDao(SessionFactory sessionFactory){
+        super(Payment.class, sessionFactory);
     }
 }
